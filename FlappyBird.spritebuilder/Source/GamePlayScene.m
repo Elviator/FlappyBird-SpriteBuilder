0e673ff0b81ed70239b872f48368f3f52687ddc0
Elviator <elviator@gmail.com>
2014-10-20 21:25:29 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -18,6 +18,15 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+        
+    }
     
 }
 
